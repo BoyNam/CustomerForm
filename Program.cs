a41d543c6a666b4438985e38cc9129f9083565b3
tontay123 <123915278+tontay123@users.noreply.github.com>
2024-03-29 12:43:59 +0700

    complete assignment

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using FormCustomer.Data;
+using FormCustomer.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<DapperDbContext>();
+builder.Services.AddScoped<ICustomerService, CustomerServiceImp>();
+
 
 var app = builder.Build();
 
@@ -22,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Customer}/{action=Index}/{id?}");
 
 app.Run();
